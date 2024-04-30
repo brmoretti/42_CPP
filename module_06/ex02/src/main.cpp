3fed4a9ad649feb62c8147aaf7ecdf6ce44103c8
Bruno Moretti <brmoretti@gmail.com>
2024-04-29 23:44:19 -0300

    module 06 done

---
+++ module_06/ex02/src/main.cpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmoretti <bmoretti@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/28 23:08:47 by bmoretti          #+#    #+#             */
+/*   Updated: 2024/04/28 23:31:36 by bmoretti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+Base*	generate(void);
+void	identify(Base* p);
+void	identify(Base& p);
+
+int main()
+{
+	Base*	base;
+
+	std::srand(std::time(NULL));
+	for (int i = 0; i < 10; i++) {
+		base = generate();
+		std::cout << "Pointer:\t";
+		identify(base);
+		std::cout << "Reference:\t";
+		identify(*base);
+		std::cout << std::endl;
+		delete base;
+	}
+	return 0;
+}
